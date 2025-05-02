999f197f14b1a3446c48a9ee63f6426857a0bfb5
Bhagyasri <bhagyasripotlapalli@gmail.com>
2025-05-02 18:16:01 +0530

    changes

--- src/server.js
+++ src/server.js
@@ -4,20 +4,20 @@ import app from "../src/app.js";
 import dotenv from "dotenv";
 
 dotenv.config();
-// import { execSync } from "child_process";
-// const getCurrentBranch = () => {
-//   return execSync("git rev-parse --abbrev-ref HEAD").toString().trim();
-// };
+import { execSync } from "child_process";
+const getCurrentBranch = () => {
+  return execSync("git rev-parse --abbrev-ref HEAD").toString().trim();
+};
 
 
-// const currentBranch = getCurrentBranch();
-// if (currentBranch === "prod") {
-//     dotenv.config({ path: "./.env.production" });
-//     console.log("Loaded production environment variables.");
-// } else {
+const currentBranch = getCurrentBranch();
+if (currentBranch === "prod") {
+    dotenv.config({ path: "./.env.production" });
+    console.log("Loaded production environment variables.");
+} else {
     dotenv.config({ path: "./.env.development" });
     console.log("Loaded development environment variables.");
-//} 
+} 
 console.log('PORT:', process.env.PORT);  
 
 const DB = process.env.DATABASE.replace(
